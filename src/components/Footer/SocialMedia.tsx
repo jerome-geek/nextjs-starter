014bf7e5cac3a581a091df712e77eb30b735596c
jerome <jerome@geekstudio.co.kr>
2022-11-21 15:56:06 +0900

    :sparkles: feat: Footer Component 추가
     - `getStaticProps`를 이용해서 서버에서 데이터를 주입

---
+++ src/components/Footer/SocialMedia.tsx
@@ -0,0 +1,49 @@
+import {
+    FC,
+    HTMLAttributes,
+    ReactNode,
+    HTMLAttributeAnchorTarget,
+} from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+import media from 'utils/styles/media';
+
+interface SocialMediaProps extends HTMLAttributes<HTMLUListElement> {
+    socialMedia: {
+        name: string;
+        url: string;
+        target: HTMLAttributeAnchorTarget;
+        icon: ReactNode;
+    }[];
+}
+
+const SocialMediaList = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    ${media.small} {
+        margin-bottom: 12px;
+    }
+`;
+
+const SocialMediaListItem = styled.li`
+    padding: 0 5px;
+`;
+
+const SocialMedia: FC<SocialMediaProps> = ({ socialMedia }) => (
+    <SocialMediaList>
+        {socialMedia.map(({ name, url, target, icon }) => {
+            return (
+                <SocialMediaListItem key={name}>
+                    <Link href={{ pathname: url }} target={target}>
+                        {icon}
+                    </Link>
+                </SocialMediaListItem>
+            );
+        })}
+    </SocialMediaList>
+);
+
+export default SocialMedia;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,15 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { NextSeo } from 'next-seo';
 
+import Header from 'components/Common/Header';
+import Footer from 'components/Common/Footer';
 import styles from '../styles/Home.module.css';
+import mall from 'api/mall';
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <div>
+            <Header />
             <NextSeo
                 title='home'
                 description='home description'
@@ -69,23 +73,7 @@ export default function Home() {
                 </div>
             </main>
 
-            <footer className={styles.footer}>
-                <a
-                    href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    Powered by{' '}
-                    <span className={styles.logo}>
-                        <Image
-                            src='/vercel.svg'
-                            alt='Vercel Logo'
-                            width={72}
-                            height={16}
-                        />
-                    </span>
-                </a>
-            </footer>
+            <Footer />
         </div>
     );
 }
