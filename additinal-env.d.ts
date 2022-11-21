3a8f89c6619f90af7237810b3f88a84269db295f
jerome <jerome@geekstudio.co.kr>
2022-11-21 14:08:39 +0900

    :package: add: 패키지 추가 & 타입 추가
    - `@fxts/core` 추가
    - `@svgr/webpack` 추가
    - `next-seo` 추가
    - `qs` 추가
    - `usehooks-ts` 추가

---
+++ additinal-env.d.ts
@@ -0,0 +1,39 @@
+type Nullable<T> = T | null;
+
+interface Sort {
+    orderBy: ORDER_BY;
+    orderDirection: ORDER_DIRECTION;
+}
+
+interface Paging {
+    pageNumber: number;
+    pageSize: number;
+    hasTotalCount: boolean;
+}
+
+interface SearchDate {
+    startYmd: string;
+    endYmd: string;
+}
+
+interface ItemList<T> {
+    totalCount: number;
+    items: T[];
+}
+
+interface ShopByErrorResponse {
+    code: string;
+    detail: {
+        time: string;
+        extra: Nullable<string>;
+    };
+    error: string;
+    message: string;
+    path: string;
+    status: number;
+}
+
+interface MemberIntegrationResponse {
+    code: string;
+    message: string;
+}
