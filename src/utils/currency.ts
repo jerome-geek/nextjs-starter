44bd5369f702e154d5b4cd7b3e9b2bdcabd1344e
jerome <jerome@geekstudio.co.kr>
2022-11-23 12:00:08 +0900

    :package: add: currency.js 추가 & 통화 관련 유틸 추가

---
+++ src/utils/currency.ts
@@ -0,0 +1,21 @@
+import currency from 'currency.js';
+
+const KRW = (
+    value: currency.Any,
+    options: currency.Options = {
+        symbol: '원',
+        precision: 0,
+        pattern: `# !`,
+        negativePattern: `- # !`,
+    },
+) => currency(value, { ...options });
+
+const USD = (value: currency.Any) => currency(value);
+
+const JPY = (value: currency.Any) =>
+    currency(value, { precision: 0, symbol: '¥' });
+
+const EURO = (value: currency.Any) =>
+    currency(value, { symbol: '€', decimal: ',', separator: '.' });
+
+export { KRW, USD, JPY, EURO };
