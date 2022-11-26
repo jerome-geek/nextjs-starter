d12148b625b251ee025bc7e438ae084c800c21f7
jerome <jerome@geekstudio.co.kr>
2022-11-26 23:55:34 +0900

    :sparkles: feat: useCouponData hooks 추가

--- src/hooks/queries/index.ts
+++ src/hooks/queries/index.ts
@@ -1,5 +1,6 @@
 import useBanners from 'hooks/queries/useBanners';
+import useCouponData from 'hooks/queries/useCouponData';
 import useMall from 'hooks/queries/useMall';
 import useFavoriteKeywords from 'hooks/queries/useFavoriteKeywords';
 
-export { useBanners, useMall, useFavoriteKeywords };
+export { useBanners, useCouponData, useMall, useFavoriteKeywords };
