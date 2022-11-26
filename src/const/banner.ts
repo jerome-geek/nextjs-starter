a47c72504434817ad9376fcc34881196e16be4cb
jerome <jerome@geekstudio.co.kr>
2022-11-26 23:26:16 +0900

    :sparkles: add: 상수 관련 파일 추가

--- src/const/banner.ts
+++ src/const/banner.ts
@@ -1,12 +1,12 @@
 const BANNER = {
-    MAIN_BAND_BANNER: process.env.NODE_ENV === 'development' ? '006' : '',
-    MAIN_WEB_BANNER: process.env.NODE_ENV === 'development' ? '000' : '',
-    MAIN_MOBILE_BANNER: process.env.NODE_ENV === 'development' ? '005' : '',
-    MAIN_CATEGORY_BANNER: process.env.NODE_ENV === 'development' ? '004' : '',
-    MAIN_ETC_BANNER: process.env.NODE_ENV === 'development' ? '008' : '',
-    VC_MANAGER: process.env.NODE_ENV === 'development' ? '009' : '',
-    VC_MANUAL: process.env.NODE_ENV === 'development' ? '010' : '',
-    VC_GOLF_COURSE: process.env.NODE_ENV === 'development' ? '011' : '',
+    MAIN_BAND_BANNER: process.env.NODE_ENV === 'development' ? '006' : '006',
+    MAIN_WEB_BANNER: process.env.NODE_ENV === 'development' ? '000' : '000',
+    MAIN_MOBILE_BANNER: process.env.NODE_ENV === 'development' ? '005' : '005',
+    MAIN_CATEGORY_BANNER: process.env.NODE_ENV === 'development' ? '004' : '004',
+    MAIN_ETC_BANNER: process.env.NODE_ENV === 'development' ? '008' : '008',
+    VC_MANAGER: process.env.NODE_ENV === 'development' ? '009' : '009',
+    VC_MANUAL: process.env.NODE_ENV === 'development' ? '010' : '010',
+    VC_GOLF_COURSE: process.env.NODE_ENV === 'development' ? '011' : '011',
 };
 
 export default BANNER;
